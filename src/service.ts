81ffb51f2646f7eebbb59f9ff2056f020fd33495
Rkozik <rkozik1989@gmail.com>
2023-03-31 12:53:18 -0500

    Added in versioning.

--- src/service.ts
+++ src/service.ts
@@ -4,6 +4,7 @@ const app: express = express();
 import dotenv from 'dotenv';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { LIB_VERSION } from './version.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,23 +13,24 @@ dotenv.config({path:__dirname+'/./../../.env'});
 
 const port = process.env.PORT;
 
-
 app.post('/', (req, res) => {
   const message:string = encodeURIComponent(req.query.message);
 
   fetch("https://postman-echo.com/get?"+message)
     .then(
-      response => response.json() // .json(), .blob(), etc.
+      response => response.json()
     ).then((result) => {
       res.send({
         'echo': result,
         'env': process.env.VERSION,
-        'timestamp':  Date.now()
+        'timestamp':  Date.now(),
+        'version': LIB_VERSION
       });
     }
   );
+
 });
 
 app.listen(port, () => {
-  console.log("Service listening on port "+port+"!");
+  console.log("Service v"+LIB_VERSION+" listening on port "+port+"!");
 });
